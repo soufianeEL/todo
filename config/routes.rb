22451883b19caa19702e8bcad3bab0edd1100cbd
soufiane <soufianeelhamchi@gmail.com>
2014-03-15 12:48:14 +0000

    modif readme 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TodoApp::Application.routes.draw do
+  get "todos/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
