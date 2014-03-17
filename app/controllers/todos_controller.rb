41e226aefcb530ded588dc30fc2d4ea5af8269b9
soufiane <soufianeelhamchi@gmail.com>
2014-03-17 15:53:16 +0000

    todo model

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,6 +2,7 @@ class TodosController < ApplicationController
   def index
 	#@todo_item1 = "recherche et analyse"
 	#@todo_item2 = "conception"
-	@todo_array = ["recherche et analyse", "conception", "validation", "developpement"]
+	#@todo_array = ["recherche et analyse", "conception", "validation", "developpement"]
+	@todo_items = Todo.all
   end
 end
